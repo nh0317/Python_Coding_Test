d68184282623e82feda9d884c4381e35653e5f35
KNH <nh2004@naver.com>
2021-07-01 01:55:43 +0900

    백준 18352
    다익스트라 알고리즘 이용

---
+++ 1week/18352.py
@@ -0,0 +1,42 @@
+import sys
+import heapq
+
+def Dijkstra(graph, start):
+    distance = [float('inf')] * (V + 1)
+    distance[start] = 0
+
+    PQ = []
+    heapq.heappush(PQ, (0, start))
+
+    while PQ:
+        weight, next = heapq.heappop(PQ)
+        for v in graph[next]:
+            sum = weight + 1
+            if (sum < distance[v]):
+                distance[v] = sum
+                heapq.heappush(PQ, (sum, v))
+
+    return distance
+
+if __name__ =='__main__':
+    V, E, min, start =map(int, sys.stdin.readline().split())
+    graph =[[]for i in range(V+1)]
+
+    for i in range(E):
+        A,B=map(int, sys.stdin.readline().split())
+        graph[A].append(B)
+
+    distance=Dijkstra(graph,start)
+
+    result = []
+    for i in range(1, V + 1):
+        if (distance[i] == min):
+            result.append(i)
+
+    sorted(result)
+    if not result:
+        print(-1)
+    else:
+        for r in result:
+            print(r)
+
