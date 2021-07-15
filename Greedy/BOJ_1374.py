245de7d6b00012e16cf5438b73950056171a4594
KNH <nh2004@naver.com>
2021-07-16 02:06:26 +0900

    Greedy 1374 수정

--- Greedy/BOJ_1374.py
+++ Greedy/BOJ_1374.py
@@ -3,22 +3,26 @@ import heapq
 if __name__=='__main__':
     input=sys.stdin.readline
     N=int(input())
-    room=[]
+    inClass=[]
+    lecture=[]
 
     for i in range(N):
         r, s,e=map(int,input().split())
-        heapq.heappush(room,(s,e,r))
+        heapq.heappush(lecture,(s,e,r))
     need=0
-    inClass=[]
-    while room:
+
+    while lecture:
         if inClass:
-            if inClass[0][0] > room[0][0] :
+            # 진행 중인 수업이 안 끝난 경우
+            if inClass[0][0] > lecture[0][0]:
                 need+=1
+
             else:
                 heapq.heappop(inClass)
         else:
             need+=1
-        if room:
-            s,e,r=heapq.heappop(room)
-            heapq.heappush(room,(e,s,r))
+        if lecture:
+            s,e,r= heapq.heappop(lecture)
+            heapq.heappush(inClass,(e,s,r))
+
     print(need)
