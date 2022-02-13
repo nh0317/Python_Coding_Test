65a36e7eb6f03f1bc055120d060fb45459967824
KNH <nh2004@naver.com>
2022-02-13 16:39:53 +0900

    백준 14502 파이썬
    리팩토링

--- study1/BOJ_14502.py
+++ study1/BOJ_14502.py
@@ -1,7 +1,6 @@
 import sys
 import copy
 import itertools
-from collections import deque
 
 max_safe=-1
 
@@ -26,32 +25,45 @@ def cnt_safe_zone(maze):
     return cnt
 
 def bfs():
-    maze=copy.deepcopy(initMaze)
-    for y,x in virus:
-        spread(y,x,maze)
-    global max_safe
-    max_safe=max(max_safe,cnt_safe_zone(maze))
+    walls = list(itertools.combinations(blank, 3))
+    # print(blank)
+    for i in range( len(walls)):
+        maze=copy.deepcopy(initMaze)
+        for x,y in walls[i]:
+            maze[x][y]=1
+        # print(maze)
+        for y,x in virus:
+            spread(y,x,maze)
+        global max_safe
+        max_safe=max(max_safe,cnt_safe_zone(maze))
 
-def select_walls(cnt):
-    if cnt ==3:
-        bfs()
-    else:
-        for i in range(N):
-            for j in range(M):
-                if initMaze[i][j]==0:
-                    initMaze[i][j]=1
-                    select_walls(cnt+1)
-                    initMaze[i][j]=0
+# def select_walls(i):
+
+# def select_walls(cnt):
+#     if cnt ==3:
+#         bfs()
+#     else:
+#         for i in range(N):
+#             for j in range(M):
+#                 if initMaze[i][j]==0:
+#                     initMaze[i][j]=1
+#                     select_walls(cnt+1)
+#                     initMaze[i][j]=0
 
 input=sys.stdin.readline
+
 # 0 : 빈칸, 1: 벽, 2: 바이러스
 N,M=map(int,input().rsplit())
 initMaze=[list(map(int,input().rsplit())) for _ in range(N)]
 virus=[]
+blank=[]
 for i in range(N):
     for j in range(M):
         if(initMaze[i][j]==2):
             virus.append([i,j])
-select_walls(0)
+        if(initMaze[i][j]==0):
+            blank.append([i,j])
+# select_walls(0)
+bfs()
 print(max_safe)
 
