713ad41ac2262fc9daed3e0afa4f0780c4e53ec8
김나현 <nh2004@naver.com>
2022-08-01 14:18:10 +0900

    [Gold V] Title: 잠수함식별, Time: 192 ms, Memory: 119492 KB -BaekjoonHub

--- 백준/Gold/2671. 잠수함식별/잠수함식별.py
+++ 백준/Gold/2671. 잠수함식별/잠수함식별.py
@@ -5,15 +5,10 @@ input = sys.stdin.readline
 sound = input().rstrip()
 
 def judge_sound(sound):
-    p100 = re.compile('((01)*(100+1+)+)*')
-    if p100.fullmatch(sound) and str(p100.fullmatch(sound).group()) == sound:
-        return 'SUBMARINE'
-
-    p01 = re.compile('((100+1+)*(01)+)*')
-    if p01.fullmatch(sound) and str(p01.fullmatch(sound).group()) == sound:
+    p = re.compile('((01)|(100+1+))+')
+    if p.fullmatch(sound) and str(p.fullmatch(sound).group()) == sound:
         return 'SUBMARINE'
     return 'NOISE'
-
 
 print(judge_sound(sound))
 
