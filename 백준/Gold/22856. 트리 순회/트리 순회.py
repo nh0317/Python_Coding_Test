e9862edd7f3fb57629030cb18d570ea35ba1194c
김나현 <nh2004@naver.com>
2022-08-08 12:56:57 +0900

    [Gold IV] Title: 트리 순회, Time: 2540 ms, Memory: 1538056 KB -BaekjoonHub

--- 백준/Gold/22856. 트리 순회/트리 순회.py
+++ 백준/Gold/22856. 트리 순회/트리 순회.py
@@ -10,6 +10,7 @@ def last_in_order(node):
         return
     last_in_order(tree[node][0])
     last = node
+    # print(last)
     last_in_order(tree[node][1])
 
 
@@ -24,32 +25,28 @@ def in_order_travel(tree):
     while stack:
         u = stack.pop()
         path += 1
+        
+        # 문제의 조건 
+        # 1. 왼쪽 노드가 있고, 방문하지 않은 경우 
         if tree[u][0] != -1 and not visited[tree[u][0]]:
             stack.append(tree[u][0])
+            
+        # 2. 오른쪽 노드가 있고 방문하지 않은 경우 
         elif tree[u][1] != -1 and not visited[tree[u][1]]:
             stack.append(tree[u][1])
+            
+        # 3. 중위 순회와 같은 노드로 이동한 경우 탐색 종료 
         elif u == last:
             return path -1
+        
+        # 4. 부모 노드로 이동 
         elif parents[u] != -1:
             stack.append(parents[u])
-
+        
+        # 중위 순회 시 방문 조건 
+        # leaf 노드 이거나 왼쪽 노드를 방문한 경우 
         if (tree[u][0] == -1 and tree[u][1] == -1) or visited[tree[u][0]]:
             visited[u] = True
-                # n -= 1
-
-        # if n == 0:
-        #     return path - 1
-        #
-        # check = False
-        # for i in range(1, -1, -1):
-        #     v = tree[u][i]
-        #     if v != -1 and not visited[v]:
-        #         stack.append(v)
-        #         check = True
-        #
-        # if not check and parents[u] != -1:
-        #     if n > 1 or not visited[parents[u]]:
-        #         stack.append(parents[u])
 
     return path
 
@@ -69,5 +66,5 @@ for _ in range(1,N+1):
         parents[v] = i
         
 last = 0
-last_in_order(1)
+last_in_order(1) # 중위 순회에서 마지막 노드 찾기
 print(in_order_travel(tree))
