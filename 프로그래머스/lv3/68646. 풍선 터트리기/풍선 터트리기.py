7e8e0f58e6df569917f7311278a20595e10cbe38
김나현 <nh2004@naver.com>
2022-08-13 18:11:28 +0900

    [level 3] Title: 풍선 터트리기, Time: 601.84 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -1,41 +1,19 @@
-# def check(a, start, end, i):
-#     cnt = 0    
-#     if start < i:
-#         min1 = min(a[start:i])
-#         if a[i] > min1:
-#             cnt += 1
-        
-#     if i+1 < end:
-#         min2 = min(a[i+1:end])
-#         if a[i] > min2:
-#             cnt += 1
-#     return True if cnt <= 1 else False
-
-
-
 def solution(a):
     answer = 0
     
-    left = [a[0] for _ in range(len(a))]
-    minn_left = a[0]
+    left = [float('inf') for _ in range(len(a))]
+    right = [float('inf') for _ in range(len(a))]
+    minn_left, minn_right = float('inf'), float('inf')
     
-    right = [a[len(a)-1] for _ in range(len(a))]
-    minn_right = a[len(a)-1]
-    
-    for i in range(1,len(a)):
-        j = len(a) - i
-        
+    for i in range(len(a)):
         if minn_left > a[i]:
             minn_left = a[i]
+        left[i] = minn_left
             
-        if left[i] > minn_left:
-            left[i] = minn_left
-            
-        if minn_right > a[j]:
-            minn_right = a[j]
-            
-        if right[j] > minn_right:
-            right[j] = minn_right
+    for i in range(len(a)-1, -1, -1):
+        if minn_right > a[i]:
+            minn_right = a[i]
+        right[i] = minn_right
     
     for i in range(len(a)):
         if not (right[i] < a[i]  and left[i] < a[i]):
