713e7ad5107b8846e7a079511412566c32fbc4c1
김나현 <nh2004@naver.com>
2022-09-05 16:42:00 +0900

    [level 3] Title: 징검다리 건너기, Time: 157.56 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,30 +1,29 @@
-from collections import Counter
+def check(stones,k,n):
+    temp = k
+    for stone in stones:
+        if stone < n:
+            temp -= 1
+        else:
+            temp = k
+
+        if temp == 0:
+            return False
+    return True
 
 def solution(stones, k):
-    if k == len(stones):
-        return max(stones)
-    elif k == 1:
-        return min(stones)
-    check = sorted(stones, reverse=True)
-    if check == stones:
-        return stones[-k]
-    maxx = 0
-    i = 0
-    minn =float('inf')
-    while i < len(stones)-k+1:
-        nextt = i
-        for j in range(k):
-            if maxx <= stones[i+j]:
-                maxx = stones[i+j]
-                nextt = i+j
-        
-        # dp[i] = maxx
-        if maxx < minn:
-            minn = maxx
-        
-        i = 1 + nextt
-        if i < len(stones):
-            maxx = stones[i]
-        else: break
-            
-    return minn
+    l = 0
+    r = 200000000
+    while l<r-1:
+        n = (l+r)//2
+        canJump = check(stones,k,n)
+
+        if canJump:
+            l = n
+        else:
+            r = n
+
+    rok = check(stones,k,r)
+    if rok:
+        return r
+    else:
+        return l
