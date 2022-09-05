38f0429559c80bdadd3e5c8ca331fbee1fe33830
김나현 <nh2004@naver.com>
2022-09-05 16:46:20 +0900

    [level 3] Title: 징검다리 건너기, Time: 21.77 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,29 +1,58 @@
-def check(stones,k,n):
-    temp = k
-    for stone in stones:
-        if stone < n:
-            temp -= 1
-        else:
-            temp = k
+# def check(stones,k,n):
+#     temp = k
+#     for stone in stones:
+#         if stone < n:
+#             temp -= 1
+#         else:
+#             temp = k
 
-        if temp == 0:
-            return False
-    return True
+#         if temp == 0:
+#             return False
+#     return True
+
+# def solution(stones, k):
+#     l = 0
+#     r = 200000000
+#     while l<r-1:
+#         n = (l+r)//2
+#         canJump = check(stones,k,n)
+
+#         if canJump:
+#             l = n
+#         else:
+#             r = n
+
+#     rok = check(stones,k,r)
+#     if rok:
+#         return r
+#     else:
+#         return l
 
 def solution(stones, k):
-    l = 0
-    r = 200000000
-    while l<r-1:
-        n = (l+r)//2
-        canJump = check(stones,k,n)
+    if k == len(stones):
+        return max(stones)
+    elif k == 1:
+        return min(stones)
+    check = sorted(stones, reverse=True)
+    if check == stones:
+        return stones[-k]
+    maxx = 0
+    i = 0
+    minn =float('inf')
+    
+    while i < len(stones)-k+1:
+        nextt = i
+        for j in range(k):
+            if maxx <= stones[i+j]:
+                maxx = stones[i+j]
+                nextt = i+j
 
-        if canJump:
-            l = n
-        else:
-            r = n
+        if maxx < minn:
+            minn = maxx
 
-    rok = check(stones,k,r)
-    if rok:
-        return r
-    else:
-        return l
+        i = 1 + nextt
+        if i < len(stones):
+            maxx = stones[i]
+        else: break
+
+    return minn
