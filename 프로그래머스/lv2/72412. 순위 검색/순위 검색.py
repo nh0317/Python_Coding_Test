88fc7234f940835e28d102a67738c434172c3849
김나현 <nh2004@naver.com>
2022-09-08 02:18:23 +0900

    [level 2] Title: 순위 검색, Time: 1628.44 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -0,0 +1,49 @@
+import re
+import itertools
+from collections import defaultdict
+
+def solution(infos, querys):
+    answer = []
+    info_dict = defaultdict(list)
+    
+    for info in infos:
+        info = info.split(' ')
+        for product in set(itertools.product([True, False], repeat = 4)):
+            query = []
+            for i, exist in enumerate(product):
+                if exist:
+                    query.append(info[i])
+                else: query.append('-')
+                
+            info_dict[' and '.join(query)].append(int(info[-1]))
+    
+    for q in info_dict.keys():
+        info_dict[q] = sorted(info_dict[q], reverse=True)
+    
+    for query in querys:
+        point = int(re.findall(r'[0-9]+', query)[0])
+        query = query.replace(' '+str(point),"")
+        if info_dict[query]:    
+            start = 0
+            end = len(info_dict[query])-1
+            mid = (start + end)//2
+            while start <= end:
+                mid = (start + end)//2
+                if mid >= len(info_dict[query]):
+                    mid = -1
+                    break
+                if info_dict[query][mid] >= point:
+                    start = mid + 1
+                elif info_dict[query][mid] < point:
+                    end = mid - 1
+                    
+            if mid < len(info_dict[query]) and info_dict[query][mid] < point:
+                mid -= 1
+            answer.append(mid+1)
+        else: 
+            answer.append(0)
+    
+    
+    
+                
+    return answer
