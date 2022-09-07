20321d5dcdf2342dde50b6752985d75d65daf279
김나현 <nh2004@naver.com>
2022-09-08 02:28:38 +0900

    [level 2] Title: 순위 검색, Time: 1217.08 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -1,6 +1,7 @@
 import re
 import itertools
 from collections import defaultdict
+import bisect 
 
 def solution(infos, querys):
     answer = []
@@ -18,28 +19,30 @@ def solution(infos, querys):
             info_dict[' and '.join(query)].append(int(info[-1]))
     
     for q in info_dict.keys():
-        info_dict[q] = sorted(info_dict[q], reverse=True)
+        # info_dict[q] = sorted(info_dict[q], reverse=True)
+        info_dict[q] = sorted(info_dict[q])
     
     for query in querys:
         point = int(re.findall(r'[0-9]+', query)[0])
         query = query.replace(' '+str(point),"")
         if info_dict[query]:    
-            start = 0
-            end = len(info_dict[query])-1
-            mid = (start + end)//2
-            while start <= end:
-                mid = (start + end)//2
-                if mid >= len(info_dict[query]):
-                    mid = -1
-                    break
-                if info_dict[query][mid] >= point:
-                    start = mid + 1
-                elif info_dict[query][mid] < point:
-                    end = mid - 1
+#             start = 0
+#             end = len(info_dict[query])-1
+#             mid = (start + end)//2
+#             while start <= end:
+#                 mid = (start + end)//2
+#                 if mid >= len(info_dict[query]):
+#                     mid = -1
+#                     break
+#                 if info_dict[query][mid] >= point:
+#                     start = mid + 1
+#                 elif info_dict[query][mid] < point:
+#                     end = mid - 1
                     
-            if mid < len(info_dict[query]) and info_dict[query][mid] < point:
-                mid -= 1
-            answer.append(mid+1)
+#             if mid < len(info_dict[query]) and info_dict[query][mid] < point:
+#                 mid -= 1
+            mid = bisect.bisect_left(info_dict[query], point)
+            answer.append(len(info_dict[query]) - mid)
         else: 
             answer.append(0)
     
