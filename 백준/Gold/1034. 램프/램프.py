34a8ce70acf2b454a7819404cf05876efd5e06cd
김나현 <nh2004@naver.com>
2022-10-03 19:49:04 +0900

    [Gold IV] Title: 램프, Time: 140 ms, Memory: 116024 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.py
+++ 백준/Gold/1034. 램프/램프.py
@@ -1,25 +1,24 @@
 import sys
-from collections import Counter
+from collections import Counter, defaultdict
 
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
 lamps = []
-checks = []
+checks = defaultdict(int)
 dp = [0 for _ in range(M+1)]
 m=0
 for i in range(N):
     row = input().rstrip()
     lamps.append(list(map(int, row)))
-    checks.append(int(row, 2))
+    checks[row] += 1
 
 K = int(input())
 
-checks = Counter(checks)
 maxx = 0
 for key in checks.keys():
-    k=format(key,'0'+str(len(lamps[0]))+'b').count('0')
+    k=key.count('0')
     if k <= K and k %2 == K %2:
         maxx = max(maxx, checks[key])
 
