64eb23ee3ed5d70c82c8c68b6ea1a131a33d57da
김나현 <nh2004@naver.com>
2022-10-04 11:43:39 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 432 ms, Memory: 119016 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
@@ -1,38 +1,13 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
 
 N, W = map(int, input().split())
 
-def bfs():
-    global graph
-    q = deque([1])
-    visited = [False for _ in range(len(graph))]
-    visited[1] = True
-    leaves = 0
-
-    while q:
-        u = q.popleft()
-
-        is_leaf = True
-        for v in graph[u]:
-            if not visited[v]:
-                q.append(v)
-                visited[v] = True
-                is_leaf = False
-        if is_leaf:
-            leaves+=1
-    return leaves
-
-
-
-graph = [[] for _ in range(N+1)]
-
+nodes = [0 for _ in range(N+1)]
+nodes[1] = 1
 for _ in range(N-1):
     u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    nodes[u] += 1
+    nodes[v] += 1
 
-leaves = bfs()
-print(W/leaves)
+print(W/nodes.count(1))
